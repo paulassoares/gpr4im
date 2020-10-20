ffa61f3b0be73223966e8c1dd9a242b79006f210
Paula <soares.paulasahd@gmail.com>
2020-10-20 16:30:20 +0100

    fixed zeff for smoothing, added LoS function, added isolating beam exercise

--- load_datacube.py
+++ load_datacube.py
@@ -2,12 +2,14 @@ import numpy as np
 
 from ps_eor import psutil, datacube, pspec
 
+
 '''
 This script contains code for how to convert a data cube in image format to a data cube in visibility format,
 specifically the CartDataCube object from ps_eor required to run GPR.
 '''
 
-def generateCartDataCube(image_cube, vmin, vmax, df, nx, ny, ra_width, dec_width, mean_center=True):
+
+def generateCartDataCube(image_cube, vmin, vmax, df, nx, ny, ra_width, dec_width, mean_center=True, invert=False):
 
     '''
     Convert image data cube to CartDataCube visibility cube in order to run GPR.
@@ -22,6 +24,7 @@ def generateCartDataCube(image_cube, vmin, vmax, df, nx, ny, ra_width, dec_width
     ra_width, dec_width: width of sky in RA and Dec directions (deg)
     mean_center: if True, do mean centering for each frequency slice after 
     Fourier transforming, if False, don't
+    invert: whether or not to invert the frequency axis
 
     OUTPUT:
     a CartDataCube object (see datacube.py in ps_eor repo)
@@ -38,6 +41,7 @@ def generateCartDataCube(image_cube, vmin, vmax, df, nx, ny, ra_width, dec_width
     # convert to correct shape for running GPR [Nz, Npix]:
     axes = np.shape(vis_cube)
     vis_cube = np.reshape(vis_cube,(axes[0], axes[1]*axes[2]))
+    if invert == True: vis_cube = vis_cube[::-1]
     
     # mean center the data:
     if mean_center==True:
@@ -60,3 +64,33 @@ def generateCartDataCube(image_cube, vmin, vmax, df, nx, ny, ra_width, dec_width
     vis_cartdatacube = datacube.CartDataCube(vis_cube, uu, vv, freqs, meta)
 
     return vis_cartdatacube
+
+
+def LoSpixels(image_cube, mean_center=True):
+
+    '''
+    Convert image data cube from [Nx, Ny, Nz] to [Nz, Npix] format.
+
+    INPUTS:
+    image_cube: input data cube in image form, with shape [Nx, Ny, Nz] where Nz
+    mean_center: if True, do mean centering for each frequency slice after 
+    
+    OUTPUT:
+    a CartDataCube object (see datacube.py in ps_eor repo)
+    '''
+
+    # swap axes to [Nz, Nx, Ny] for converting to visibilities:
+    image_cube = np.swapaxes(image_cube,0,1)
+    image_cube = np.swapaxes(image_cube,0,2)
+
+    # conver to LoS pixels format [Nz, Npix]
+    axes = np.shape(image_cube)
+    image_LoSpixels = np.reshape(image_cube,(axes[0], axes[1]*axes[2]))
+    
+    # mean center the data:
+    if mean_center==True:
+        nz = np.shape(image_LoSpixels)[0]
+        for i in range(nz):
+            image_LoSpixels[i] = image_LoSpixels[i] - np.mean(image_LoSpixels[i])
+
+    return image_LoSpixels
